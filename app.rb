d414aff642446d3666bf369d97fc1755a8a1db51
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-03 18:03:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/' do
     "My name is Dillorom"
   end
+
+  get '/' do
+    "My hometown is Nurota"
+  end
+
+  get '/' do
+    "My favorite-song is Paradise"
+  end
 end
